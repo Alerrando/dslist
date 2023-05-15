67f0b4de56a0659b538a918f01126bc1fd365d80
Alerrando Breno <alerrando2@gmail.com>
2023-05-15 00:37:19 -0300

    Game repository, controller

---
+++ src/main/java/com/brenoalerrando/dslist/services/GameService.java
@@ -0,0 +1,21 @@
+package com.brenoalerrando.dslist.services;
+
+import com.brenoalerrando.dslist.dto.GamerMinDTO;
+import com.brenoalerrando.dslist.entities.Game;
+import com.brenoalerrando.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+    @Autowired
+    private GameRepository gameRepository;
+    public List<GamerMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        List<GamerMinDTO> dto = result.stream().map(x -> new GamerMinDTO(x)).toList();
+        return dto;
+    }
+}
